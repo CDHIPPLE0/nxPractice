cc8f4eaf8dd7824051224e33975a70509b38e867
Clinton Hipple <clintondhipple@hotmail.com>
2021-02-24 13:52:24 -0600

    initial commit

---
+++ apps/learning-the-tools-app/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'learning-the-tools-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/learning-the-tools-app',
+};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/learning-the-tools-app', '<rootDir>/apps/api'],
+};
